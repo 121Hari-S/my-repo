2fd458daffdc82e987cf3e3ea2aee3bcab8a0125
Hariprasath <166097172+121Hari-S@users.noreply.github.com>
2024-04-17 12:43:29 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,16 @@
+class Array
+{
+	public static void main( String[] args )
+	{
+		
+		String s="hariprasath";
+		int[] num ={100,200,300};
+
+
+
+		System.out.println( s.length());
+		System.out.println("Integer array length is " + num.length );
+		System.out.println(num[0]+","+num[1]);
+		System.out.println(s);
+	}
+}
